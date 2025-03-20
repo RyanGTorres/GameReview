4a02e1ab37145fe145a14db76de08e04e762c2c9
RyanGTorres <ryryan009@gmail.com>
2025-03-20 11:09:37 -0300

    first commit

---
+++ src/test/java/com/gamereview/GameReview/GameReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamereview.GameReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
